28233811bc1c783e611bdd2aeb25659d3809bb81
Gram <master_fess@mail.ru>
2018-08-31 20:37:13 +0300

    test right resolution

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -4,19 +4,31 @@ from dephell.resolver import Resolver
 
 
 def test_one():
-    resolver = Resolver.from_requirements('./tests/requirements/1.txt')
+    resolver = Resolver.from_requirements('./tests/requirements/django.txt')
     resolver.resolve()
     assert 'Django' in resolver.graph
 
 
 def test_two_different():
-    resolver = Resolver.from_requirements('./tests/requirements/2.txt')
+    resolver = Resolver.from_requirements('./tests/requirements/django-deal.txt')
     resolver.resolve()
     assert 'Django' in resolver.graph
     assert 'deal' in resolver.graph
 
 
 def test_unresolved():
-    resolver = Resolver.from_requirements('./tests/requirements/3.txt')
+    resolver = Resolver.from_requirements('./tests/requirements/django-django.txt')
     with pytest.raises(ImportError):
         resolver.resolve()
+
+
+def test_resolution():
+    resolver = Resolver.from_requirements('./tests/requirements/scipy-pandas-numpy.txt')
+    resolver.resolve()
+    assert 'pandas' in resolver.graph
+    assert 'scipy' in resolver.graph
+    assert 'numpy' in resolver.graph
+
+    assert resolver.graph['numpy'].best_release.version == '1.15.1'
+    assert resolver.graph['scipy'].best_release.version == '0.19.1'
+    assert resolver.graph['pandas'].best_release.version > '0.20.3'
