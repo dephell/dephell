bec0246ed1a2159a2331ffad641d65a9a9594aa4
Gram <master_fess@mail.ru>
2018-09-12 13:35:09 +0300

    +snapshots

--- dephell3/controllers/mutator.py
+++ dephell3/controllers/mutator.py
@@ -3,7 +3,7 @@ from itertools import product
 
 class Mutator:
     def __init__(self):
-        ...
+        self._snapshots = set()
 
     def mutate(self, graph) -> tuple:
         """Get graph with conflict and mutate one dependency.
@@ -24,8 +24,13 @@ class Mutator:
         for groups in product(*all_groups):
             yield groups
 
+    @staticmethod
+    def _make_snapshot(groups) -> tuple:
+        snapshot = sorted(group.normalized_name + str(group.number) for group in groups)
+        return tuple(snapshot)
+
     def check(self, groups):
-        ...
+        return self._make_snapshot(groups) in self._snapshots
 
     def remember(self, groups):
-        ...
+        self._snapshots.add(self._make_snapshot(groups))
