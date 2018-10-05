389d48952513cc9ce7ac0a132b865bfb51c20db1
Gram <master_fess@mail.ru>
2018-10-05 18:22:49 +0300

    +config parser and new cli

--- dephell/cli.py
+++ dephell/cli.py
@@ -1,12 +1,45 @@
-from cleo import Application
-from .commands import ConvertCommand
+import argparse
+
+from .config import Config
+from .console import Progress, output
+from .controllers import analize_conflict
+from .converters import CONVERTERS
 
 
-def run():
-    application = Application()
-    application.add(ConvertCommand())
-    application.run()
+parser = argparse.ArgumentParser(
+    description='Lock and convert dependencies between formats.',
+)
+parser.add_argument('-c', '--config', default='pyproject.toml', help='path to config file')
+parser.add_argument('-e', '--env', help='environment')
 
 
-if __name__ == '__main__':
-    run()
+def resolve(rule) -> bool:
+    loader = CONVERTERS[rule['from']['format']]
+    dumper = CONVERTERS[rule['to']['format']]
+
+    # load
+    resolver = loader.load_resolver(path=rule['from']['path'])
+
+    # resolve
+    if not loader.lock and dumper.lock:
+        with Progress().auto():
+            resolved = resolver.resolve()
+        if not resolved:
+            conflict = analize_conflict(graph=resolver.graph)
+            output.writeln('<error>CONFLICT:</error> ' + conflict)
+            return False
+
+    # dump
+    output.writeln('<info>Resolved!</info>')
+    dumper.dump(
+        path=rule['to']['path'],
+        graph=resolver.graph,
+    )
+    return True
+
+
+def main(args):
+    args = parser.parse_args(args)
+    rule = Config(args.config).get(args.env)
+    result = resolve(rule)
+    return int(not result)
---
+++ dephell/config.py
@@ -0,0 +1,50 @@
+import tomlkit
+
+
+DEFAULTS = dict(
+    pip='requirements.in',
+    piplock='requirements.txt',
+    pipfile='Pipfile',
+    pipfilelock='Pipfile.lock',
+    poetry='pyproject.toml',
+    poetrylock='pyproject.lock',
+)
+
+
+class Config:
+    def __init__(self, data: dict):
+        self._data = data
+
+    @classmethod
+    def load(cls, path: str):
+        with open(path, 'r', encoding='utf8') as stream:
+            doc = tomlkit.parse(stream.read())
+        if 'tool' not in doc or 'dephell' not in doc['tool']:
+            raise KeyError('section [tool.dephell...] not found')
+        data = dict(doc['tool']['dephell'])
+        return cls(data)
+
+    def get(self, env: str) -> dict:
+        if env not in self._data:
+            raise KeyError('environment not found')
+
+        config = self._data[env]
+        for key in ('from', 'to'):
+            if key not in config:
+                raise KeyError('key "{}" not found'.format(key))
+            config[key] = self._process_line(key=key, line=config[key])
+        return config
+
+    @staticmethod
+    def _process_line(key, line):
+        if isinstance(line, str):
+            line = dict(format=line)
+        if 'format' not in line:
+            raise KeyError('format required for "{}"'.format(key))
+        if line['format'] not in DEFAULTS:
+            raise KeyError('invalid format "{}". Available: {}'.format(
+                line['format'],
+                ', '.join(DEFAULTS),
+            ))
+        if 'path' not in line:
+            line['path'] = DEFAULTS[line['format']]
