4494ab07a4b245a2f96e65b8f9bbb16943195444
Gram <master_fess@mail.ru>
2018-10-15 16:46:17 +0300

    +poetry skeleton

--- dephell/converters/poetry.py
+++ dephell/converters/poetry.py
@@ -1,3 +1,112 @@
-from tomlkit import document, dumps
+from operator import attrgetter
+from tomlkit import parse, aot, table, document, dumps, inline_table
+from ..models import Dependency, RootDependency, Constraint
+from ..repositories import get_repo
+from .base import BaseConverter
 
-...
+
+class PoetryConverter(BaseConverter):
+    lock = False
+
+    def loads(self, content) -> RootDependency:
+        doc = parse(content)
+        deps = []
+        root = RootDependency()
+        if 'packages' in doc:
+            for name, content in doc['packages'].items():
+                deps.append(self._make_dep(root, name, content))
+        root.attach_dependencies(deps)
+        return root
+
+    def dumps(self, graph) -> str:
+        doc = document()
+        source = table()
+        source['url'] = 'https://pypi.python.org/simple'
+        source['verify_ssl'] = True
+        source['name'] = 'pypi'
+        sources = aot()
+        sources.append(source)
+        doc.add('source', sources)
+
+        deps = table()
+        for dep in sorted(graph.mapping.values(), key=attrgetter('name')):
+            if not dep.used:
+                continue
+            deps[dep.name] = self._format_dep(dep)
+        doc.add('packages', deps)
+
+        return dumps(doc)
+
+    # https://github.com/pypa/pipfile/blob/master/examples/Pipfile
+    @staticmethod
+    def _make_dep(root, name: str, content) -> Dependency:
+        """
+
+        postry fields:
+            main
+                version
+                python
+                platform
+                allows-prereleases
+                optional
+                extras
+                develop
+            git
+                git
+                branch
+                tag
+                rev
+            other
+                file
+                path
+        """
+        if isinstance(content, str):
+            return Dependency(
+                raw_name=name,
+                constraint=Constraint(root, content),
+                repo=get_repo(),
+            )
+
+        # https://github.com/sarugaku/requirementslib/blob/master/src/requirementslib/models/utils.py
+        version = content['version'] if 'version' in content else ''
+
+        # https://github.com/sarugaku/requirementslib/blob/master/src/requirementslib/models/requirements.py
+        # https://github.com/pypa/pipenv/blob/master/pipenv/project.py
+        return Dependency(
+            raw_name=name,
+            constraint=Constraint(root, version),
+            repo=get_repo(),
+            extras=set(content.get('extras', [])),
+            marker=content.get('markers'),
+        )
+
+    def _format_dep(self, dep: Dependency, *, short: bool=True):
+        if self.lock:
+            release = dep.group.best_release
+
+        result = inline_table()
+
+        if self.lock:
+            result['version'] = '==' + str(release.version)
+        else:
+            result['version'] = str(dep.constraint) or '*'
+
+        if dep.extras:
+            result['extras'] = list(sorted(dep.extras))
+        if dep.marker:
+            result['markers'] = str(dep.marker)
+
+        if self.lock:
+            result['hashes'] = []
+            for digest in release.hashes:
+                result['hashes'].append('sha256:' + digest)
+
+        # if we have only version, return string instead of table
+        if short and tuple(result.value) == ('version', ):
+            return result['version']
+
+        # do not specify version explicit
+        if result['version'] == '*':
+            del result['version']
+
+        return result
