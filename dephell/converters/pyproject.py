432f963c6e924d51effcf1291a2fcee01cbb9e84
Gram <master_fess@mail.ru>
2018-11-10 15:30:52 +0300

    +pyproject converter

--- dephell/converters/pyproject.py
+++ dephell/converters/pyproject.py
@@ -1,4 +1,37 @@
-# from pip._internal.req.req_file import process_line
+from tomlkit import parse, dumps, document
+from packaging.requirements import Requirement
+
+# app
+from ..models import Dependency, RootDependency
+from .base import BaseConverter
 
 
-# next(process_line(line, '', 0))
+class PyProjectConverter(BaseConverter):
+    lock = False
+
+    def loads(self, content: str) -> RootDependency:
+        doc = parse(content)
+        deps = []
+        root = RootDependency(name=self._get_name(content=content))
+        for req in doc['build-system']['requires']:
+            req = Requirement(req)
+            deps.append(Dependency.from_requirement(source=root, req=req))
+        root.attach_dependencies(deps)
+        return root
+
+    def dumps(self, reqs) -> str:
+        doc = document()
+        deps = []
+        for req in reqs:
+            deps.append(self._format_req(req=req))
+        doc['build-system']['requires'] = deps
+        return dumps(doc)
+
+    def _format_req(self, req):
+        line = req.name
+        if req.extras:
+            line += '[{}]'.format(','.join(req.extras))
+        line += req.version
+        if req.markers:
+            line += '; ' + req.markers
+        return line
