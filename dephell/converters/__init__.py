c6a1451934f9fce28de698e0d026cc1f42918dda
Gram <master_fess@mail.ru>
2018-11-10 15:19:31 +0300

    +setup.py reader

--- dephell/converters/__init__.py
+++ dephell/converters/__init__.py
@@ -2,11 +2,14 @@
 from .pip import PIPConverter
 from .pipfile import PIPFileConverter
 from .pipfilelock import PIPFileLockConverter
+from .setuppy import SetupPyConverter
 
 
 CONVERTERS = dict(
     pip=PIPConverter(lock=False),
     piplock=PIPConverter(lock=True),
+
     pipfile=PIPFileConverter(),
     pipfilelock=PIPFileLockConverter(),
+    setuppy=SetupPyConverter(),
 )
