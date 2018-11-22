b200394e7535dbbcded1fae27f05bcf7d075e162
Gram <master_fess@mail.ru>
2018-11-22 20:37:37 +0300

    test .tar.gz open

--- dephell/archive.py
+++ dephell/archive.py
@@ -34,7 +34,7 @@ class ArchiveStream:
 
         # extract to cache
         member = self.descriptor.getmember(str(self.member_path))
-        self.descriptor.extract(member=member, path=path)
+        self.descriptor.extract(member=member, path=str(self.cache_path))
 
         # read from cache
         with path.open(self.mode) as stream:
@@ -54,18 +54,17 @@ class ArchivePath:
         return EXTRACTORS[extension]
 
     @contextmanager
-    def descriptor(self):
+    def get_descriptor(self):
         if self._descriptor is not None:
             yield self._descriptor
-            return
-
-        with self.extractor(str(self.archive_path)) as descriptor:
-            self._descriptor = descriptor
-            try:
-                yield self._descriptor
-            except Exception:
-                self._descriptor = None
-                raise
+        else:
+            with self.extractor(str(self.archive_path)) as descriptor:
+                self._descriptor = descriptor
+                try:
+                    yield self._descriptor
+                except Exception:
+                    self._descriptor = None
+                    raise
 
     @contextmanager
     def open(self, mode='r'):
@@ -74,32 +73,32 @@ class ArchivePath:
         if path.exists():
             with path.open() as stream:
                 yield stream
-                return
-
-        with self.descriptor as descriptor:
-            yield ArchiveStream(
-                descriptor=descriptor,
-                cache_path=self.cache_path,
-                member_path=self.member_path,
-                mode=mode,
-            )
-            self._descriptor = None
+        else:
+            with self.get_descriptor() as descriptor:
+                yield ArchiveStream(
+                    descriptor=descriptor,
+                    cache_path=self.cache_path,
+                    member_path=self.member_path,
+                    mode=mode,
+                )
 
     def glob(self, pattern):
-        with self.descriptor as descriptor:
+        with self.get_descriptor() as descriptor:
             for member in descriptor.getmembers():
                 if fnmatch(name=member.name, pat=pattern):
-                    yield self.__class__(
+                    obj = self.__class__(
                         archive_path=self.archive_path,
                         cache_path=self.cache_path,
                         member_path=PurePath(member.name),
-                        _descriptor=self._descriptor,
                     )
+                    obj._descriptor = self._descriptor
+                    yield obj
 
     def __truediv__(self, key):
-        return self.__class__(
+        obj = self.__class__(
             archive_path=self.archive_path,
             cache_path=self.cache_path,
             member_path=self.member_path / key,
-            _descriptor=self._descriptor,
         )
+        obj._descriptor = self._descriptor
+        return obj
---
+++ tests/test_archive.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from dephell.archive import ArchivePath
+
+
+# def test_open_zip(tmpdir):
+#     path = ArchivePath(
+#         archive_path=Path('tests', 'requirements', 'wheel.whl'),
+#         cache_path=Path(str(tmpdir)),
+#     )
+#     subpath = path / 'dephell' / '__init__.py'
+#     with subpath.open() as stream:
+#         content = stream.read()
+#     assert 'from .controllers' in content
+
+
+def test_open_tar_gz(tmpdir):
+    path = ArchivePath(
+        archive_path=Path('tests', 'requirements', 'sdist.tar.gz'),
+        cache_path=Path(str(tmpdir)),
+    )
+    subpath = path / 'dephell-0.2.0' / 'setup.py'
+    with subpath.open() as stream:
+        content = stream.read()
+    assert 'from setuptools import' in content
