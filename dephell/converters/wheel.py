ba3f1a5a5c378215d4c51d9240a8018f6be35010
Gram <master_fess@mail.ru>
2018-11-22 17:05:25 +0300

    +wheel parser

---
+++ dephell/converters/wheel.py
@@ -0,0 +1,20 @@
+from email.parser import Parser
+
+from packaging.requirements import Requirement
+
+from .base import BaseConverter
+from ..models import Dependency, RootDependency
+
+
+class WheelConverter(BaseConverter):
+    def loads(self, content: str) -> RootDependency:
+        """Parse METADATA file from .whl archive
+        """
+        info = Parser().parsestr(content)
+        root = RootDependency(name=info.get('Name').strip())
+        deps = []
+        for req in info.get_all('Requires-Dist'):
+            req = Requirement(req)
+            deps.append(Dependency.from_requirement(source=root, req=req))
+        root.attach_dependencies(deps)
+        return root
