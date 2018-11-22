b02b2d4b72ceac7fc949d0cd299df0f11d27b4dc
Gram <master_fess@mail.ru>
2018-11-22 19:22:11 +0300

    patterns support for archive extractor

--- dephell/archive.py
+++ dephell/archive.py
@@ -1,7 +1,8 @@
-from functools import partial
-import tarfile
+from tarfile import TarFile
 from zipfile import ZipFile
 from pathlib import Path
+from typing import Optional, Iterable
+from fnmatch import fnmatch
 
 
 NEW_PATH = Path('.dephell') / 'archives'
@@ -10,17 +11,31 @@ NEW_PATH = Path('.dephell') / 'archives'
 EXTRACTORS = {
     '.zip': ZipFile,
     '.whl': ZipFile,
-    '.tar': partial(tarfile.open, mode='r:'),
-    '.tar.gz': partial(tarfile.open, mode='r:gz'),
-    '.tar.bz2': partial(tarfile.open, mode='r:bz2'),
-    '.tar.xz': partial(tarfile.open, mode='r:xz'),
+    '.tar': TarFile.taropen,
+    '.tar.gz': TarFile.gzopen,
+    '.tar.bz2': TarFile.bz2open,
+    '.tar.xz': TarFile.xzopen,
 }
 
 
-def extract(path: Path) -> Path:
+def extract(
+    path: Path,
+    patterns: Optional[Iterable] = None,
+    force: bool = False,
+) -> Path:
     new_path = NEW_PATH / path.stem.stem
+    if not force and new_path.exists:
+        return new_path
+
     extension = ''.join(path.suffixes)
     extractor = EXTRACTORS[extension]
+
     with extractor(str(path)) as archive:
-        archive.extractall(new_path)
+        if patterns is None:
+            archive.extractall(new_path)
+        else:
+            for member in archive.getmembers():
+                for pattern in patterns:
+                    if fnmatch(name=member.name, pat=pattern):
+                        archive.extract(member=member)
     return new_path
