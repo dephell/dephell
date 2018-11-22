9de1bef1faecc4f9092439b5fd5a6584372dc13a
Gram <master_fess@mail.ru>
2018-11-22 20:21:21 +0300

    +ArchivePath

--- dephell/archive.py
+++ dephell/archive.py
@@ -1,8 +1,10 @@
+from contextlib import contextmanager
 from tarfile import TarFile
 from zipfile import ZipFile
-from pathlib import Path
-from typing import Optional, Iterable
+from pathlib import Path, PurePath
 from fnmatch import fnmatch
+
+import attr
 
 
 NEW_PATH = Path('.dephell') / 'archives'
@@ -18,24 +20,86 @@ EXTRACTORS = {
 }
 
 
-def extract(
-    path: Path,
-    patterns: Optional[Iterable] = None,
-    force: bool = False,
-) -> Path:
-    new_path = NEW_PATH / path.stem.stem
-    if not force and new_path.exists:
-        return new_path
+@attr.s()
+class ArchiveStream:
+    descriptor = attr.ib()
+    cache_path = attr.ib()
+    member_path = attr.ib()
+    mode = attr.ib()
 
-    extension = ''.join(path.suffixes)
-    extractor = EXTRACTORS[extension]
+    def read(self):
+        path = self.cache_path / self.member_path
+        if path.exists():
+            raise FileExistsError('file created between open and read')
 
-    with extractor(str(path)) as archive:
-        if patterns is None:
-            archive.extractall(new_path)
-        else:
-            for member in archive.getmembers():
-                for pattern in patterns:
-                    if fnmatch(name=member.name, pat=pattern):
-                        archive.extract(member=member)
-    return new_path
+        # extract to cache
+        member = self.descriptor.getmember(str(self.member_path))
+        self.descriptor.extract(member=member, path=path)
+
+        # read from cache
+        with path.open(self.mode) as stream:
+            return stream.read()
+
+
+@attr.s()
+class ArchivePath:
+    archive_path = attr.ib()
+    cache_path = attr.ib()
+    member_path = attr.ib(factory=PurePath)
+    _descriptor = attr.ib(default=None, repr=False)
+
+    @property
+    def extractor(self):
+        extension = ''.join(self.archive_path.suffixes)
+        return EXTRACTORS[extension]
+
+    @contextmanager
+    def descriptor(self):
+        if self._descriptor is not None:
+            yield self._descriptor
+            return
+
+        with self.extractor(str(self.archive_path)) as descriptor:
+            self._descriptor = descriptor
+            try:
+                yield self._descriptor
+            except Exception:
+                self._descriptor = None
+                raise
+
+    @contextmanager
+    def open(self, mode='r'):
+        # read from cache
+        path = self.cache_path / self.member_path
+        if path.exists():
+            with path.open() as stream:
+                yield stream
+                return
+
+        with self.descriptor as descriptor:
+            yield ArchiveStream(
+                descriptor=descriptor,
+                cache_path=self.cache_path,
+                member_path=self.member_path,
+                mode=mode,
+            )
+            self._descriptor = None
+
+    def glob(self, pattern):
+        with self.descriptor as descriptor:
+            for member in descriptor.getmembers():
+                if fnmatch(name=member.name, pat=pattern):
+                    yield self.__class__(
+                        archive_path=self.archive_path,
+                        cache_path=self.cache_path,
+                        member_path=PurePath(member.name),
+                        _descriptor=self._descriptor,
+                    )
+
+    def __truediv__(self, key):
+        return self.__class__(
+            archive_path=self.archive_path,
+            cache_path=self.cache_path,
+            member_path=self.member_path / key,
+            _descriptor=self._descriptor,
+        )
--- dephell/converters/wheel.py
+++ dephell/converters/wheel.py
@@ -1,12 +1,20 @@
 from email.parser import Parser
+from pathlib import Path
 
 from packaging.requirements import Requirement
 
 from .base import BaseConverter
 from ..models import Dependency, RootDependency
+from ..archive import ArchivePath
 
 
 class WheelConverter(BaseConverter):
+    def load(self, path) -> RootDependency:
+        path = Path(str(path))
+        if path.suffix == '.whl':
+            # new_path = extract(path=path, patterns=['*METADATA'])
+            ...
+
     def loads(self, content: str) -> RootDependency:
         """Parse METADATA file from .whl archive
         """
