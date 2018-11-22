30a67e971f4081665a5fb9ac50457c58227721b7
Gram <master_fess@mail.ru>
2018-11-22 16:47:04 +0300

    +EggInfoConverter load

---
+++ dephell/converters/egginfo.py
@@ -0,0 +1,23 @@
+from email.parser import Parser
+from pathlib import Path
+
+from packaging.requirements import Requirement
+
+from .base import BaseConverter
+from ..models import Dependency, RootDependency
+
+
+class EggInfoConverter(BaseConverter):
+    def load(self, path) -> RootDependency:
+        path = Path(str(path))
+        with (path / 'PKG-INFO').open('r') as stream:
+            info = Parser().parse(stream)
+        root = RootDependency(name=info.get('Name').strip())
+
+        deps = []
+        with (path / 'requires.txt').open('r') as stream:
+            for req in stream:
+                req = Requirement(req)
+                deps.append(Dependency.from_requirement(source=root, req=req))
+        root.attach_dependencies(deps)
+        return root
