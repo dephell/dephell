c3cdfc83b6f9e26553b2c93e5a67128cccc8b41a
Gram <master_fess@mail.ru>
2018-12-07 10:37:49 +0300

    group cli args

--- dephell/config/parser.py
+++ dephell/config/parser.py
@@ -5,6 +5,9 @@ from argparse import ArgumentParser
 from ..constants import ENVS, FORMATS
 
 
+__all__ = ['parser']
+
+
 parser = ArgumentParser(
     description='Lock and convert dependencies between formats.',
 )
@@ -16,24 +19,26 @@ env_help = (
     'from given env.'
 )
 
-# config
-parser.add_argument('-c', '--config', help='path to config file')
-parser.add_argument('-e', '--env', default='main', help='environment')
+config_group = parser.add_argument_group('Configuration file')
+config_group.add_argument('-c', '--config', help='path to config file')
+config_group.add_argument('-e', '--env', default='main', help='environment')
 
-# from
-parser.add_argument('--from-format', choices=FORMATS, help='format for reading requirements')
-parser.add_argument('--from-path', help='path to input file')
-parser.add_argument('--from-env', action='append', dest='from_envs', choices=ENVS, help=env_help)
+from_group = parser.add_argument_group('Input file')
+from_group.add_argument('--from-format', choices=FORMATS, help='format for reading requirements')
+from_group.add_argument('--from-path', help='path to input file')
+from_group.add_argument('--from-env', action='append', dest='from_envs', choices=ENVS, help=env_help)
 
-# to
-parser.add_argument('--to-format', choices=FORMATS, help='output requirements file format')
-parser.add_argument('--to-path', help='path to output file')
-parser.add_argument('--to-env', action='append', dest='to_envs', choices=ENVS, help=env_help)
+to_group = parser.add_argument_group('Output file')
+to_group.add_argument('--to-format', choices=FORMATS, help='output requirements file format')
+to_group.add_argument('--to-path', help='path to output file')
+to_group.add_argument('--to-env', action='append', dest='to_envs', choices=ENVS, help=env_help)
 
-# APIs
-parser.add_argument('--warehouse', help='warehouse API URL.')
-parser.add_argument('--bitbucket', help='bitbucket API URL')
+api_group = parser.add_argument_group('APIs endpoints')
+api_group.add_argument('--warehouse', help='warehouse API URL.')
+api_group.add_argument('--bitbucket', help='bitbucket API URL')
 
-# other
-parser.add_argument('--silent', action='store_true', help='suppress any output except errors.')
-parser.add_argument('--cache', help='path to dephell cache')
+output_group = parser.add_argument_group('Console output')
+output_group.add_argument('--silent', action='store_true', help='suppress any output except errors.')
+
+other_group = parser.add_argument_group('other')
+other_group.add_argument('--cache', help='path to dephell cache')
