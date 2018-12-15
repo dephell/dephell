1e476f2c8db18a0ed7bba38ed8b7d26d8a4c6b5a
Gram <master_fess@mail.ru>
2018-12-15 11:18:18 +0300

    +range specifier

--- dephell/models/git_specifier.py
+++ dephell/models/git_specifier.py
@@ -5,3 +5,10 @@ from .git_release import GitRelease
 class GitSpecifier:
     def __contains__(self, release):
         return isinstance(release, GitRelease)
+
+    def __iadd__(self, other):
+        if hasattr(other, '_attach'):
+            attached = other._attach(self)
+            if attached:
+                return other
+        return NotImplemented
---
+++ dephell/models/range_specifier.py
@@ -0,0 +1,91 @@
+from .specifier import Specifier
+from .git_specifier import GitSpecifier
+
+
+class RangeSpecifier:
+    def __init__(self, spec=None):
+        if spec is not None:
+            self._specs = self._parse(spec)
+
+    @staticmethod
+    def _parse(spec) -> set:
+        if not isinstance(spec, (list, tuple)):
+            spec = str(spec).split(',')
+        result = set()
+        for constr in spec:
+            constr = constr.strip()
+            if constr in ('', '*'):
+                continue
+
+            # https://docs.npmjs.com/misc/semver#advanced-range-syntax
+            if ' - ' in constr:
+                left, right = constr.split(' - ', maxsplit=1)
+                result.add(Specifier('>=' + left))
+                result.add(Specifier('<=' + right))
+                continue
+            if constr[0] == '~':
+                ...
+            if constr[0] == '^':
+                ...
+
+            constr = constr.replace('.x', '.*')
+            constr = constr.replace('.X', '.*')
+            result.add(Specifier(constr))
+        return result
+
+    def attach_time(self, releases) -> bool:
+        """Attach time to all specifiers if possible
+        """
+        ok = False
+        for spec in self._specs:
+            if spec.time is None:
+                attached = spec.attach_time(releases)
+                if attached:
+                    ok = True
+        return ok
+
+    def __add__(self, other):
+        new = self.__class__()
+        new._specs = self._specs.copy()
+        attached = new._attach(other)
+        if attached:
+            return new
+        return NotImplemented
+
+    def __radd__(self, other):
+        new = self.__class__()
+        new._specs = self._specs.copy()
+        attached = new._attach(other)
+        if attached:
+            return new
+        return NotImplemented
+
+    def __iadd__(self, other):
+        attached = self._attach(other)
+        if attached:
+            return self
+        return NotImplemented
+
+    def _attach(self, other):
+        if isinstance(other, GitSpecifier):
+            self._specs.add(other)
+            return True
+        if isinstance(other, self.__class__):
+            self._specs.update(other._specs)
+            return True
+        return False
+
+    def __contains__(self, release):
+        for specifier in self._specs:
+            if release not in specifier:
+                return False
+        return True
+
+    def __str__(self):
+        return ','.join(map(str, self._specs))
+
+    def __repr__(self):
+        return '{name}({spec})'.format(
+            name=self.__class__.__name__,
+            spec=str(self),
+        )
