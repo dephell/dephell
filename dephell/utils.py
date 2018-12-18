b3b0173d5e82a3e4bf6023164c4f9cd811d98026
Gram <master_fess@mail.ru>
2018-12-18 19:15:50 +0300

    make mutator little bit faster

--- dephell/utils.py
+++ dephell/utils.py
@@ -1,6 +1,7 @@
 # built-in
 import os
 from contextlib import contextmanager
+from itertools import product
 
 
 @contextmanager
@@ -18,3 +19,26 @@ def chdir(path):
         yield
     finally:
         os.chdir(curdir)
+
+
+def lazy_product(*all_groups):
+    slices = [[] for _ in range(len(all_groups))]
+    all_groups = [iter(groups) for groups in all_groups]
+
+    while True:
+        has_tail = False
+        tail = []
+        for container, groups in zip(slices, all_groups):
+            group = next(groups, None)
+            tail.append(group)
+            if group is not None:
+                container.append(group)
+                has_tail = True
+        if not has_tail:
+            return
+
+        for groups in product(*slices):
+            for group, el in zip(groups, tail):
+                if el is not None and group == el:
+                    yield groups
+                    break
---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+from dephell.utils import lazy_product
+
+
+def test_lazy_product_order():
+    result = list(lazy_product(range(3), range(3)))
+    assert len(result) == 9
+    expected = [
+        (0, 0), (0, 1), (1, 0), (1, 1),
+        (0, 2), (1, 2), (2, 0), (2, 1), (2, 2),
+    ]
+    assert result == expected
+
+
+def test_lazy_product_tail():
+    result = list(lazy_product([0], [1, 2]))
+    assert len(result) == 2
+    expected = [(0, 1), (0, 2)]
+    assert result == expected
