b641564ca6756f5a10409bdac5b2cd467f010d01
Gram <master_fess@mail.ru>
2019-02-10 15:48:24 +0300

    test python_compat check

---
+++ tests/test_resolving/test_python_compat.py
@@ -0,0 +1,38 @@
+import pytest
+from dephell.models import Dependency, Release, Group, RangeSpecifier
+from dephell.markers import Markers
+
+
+@pytest.mark.parametrize("pdep, prel, ok", [
+    ('>=2.7',           '<=3.4',        True),
+    ('<=2.7',           '>=3.4',        False),
+
+    # open intervals
+    ('>=2.7',           '>=3.4',        True),
+    ('>=3.4',           '>=2.7',        True),
+
+    # one point intersection
+    ('>=2.7,<=3.4',     '>=3.4',        True),
+    ('>=2.7,<=3.4',     '>3.4',         False),
+    ('>=3.4',           '>=2.7,<=3.4',  True),
+    ('>3.4',            '>=2.7,<=3.4',  False),
+
+    # version isn't exist
+    ('>=2.7',           '<3.0',         True),
+    ('>=2.8',           '<3.0',         False),
+])
+def test_python_compat(pdep: str, prel: str, ok: bool):
+    dep = Dependency(
+        raw_name='pathlib2',
+        constraint=None,
+        repo=None,
+        marker=Markers(RangeSpecifier(pdep).to_marker('python_version')),
+    )
+    release = Release(
+        raw_name='pathlib2',
+        version='2.3.3',
+        time=None,
+        python=RangeSpecifier(prel),
+    )
+    dep.groups = [Group(number=1, releases=[release])]
+    assert dep.python_compat is ok
