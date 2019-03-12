25f63fa7627b77f0196483ed7cbdf77f172034bd
Gram <master_fess@mail.ru>
2019-03-12 13:33:24 +0300

    test warehouse extras

---
+++ tests/test_repositories/test_warehouse.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from dephell.repositories import WareHouseRepo
+
+
+loop = asyncio.get_event_loop()
+
+
+def test_extra():
+    repo = WareHouseRepo()
+
+    coroutine = repo.get_dependencies(name='requests', version='2.21.0')
+    deps = loop.run_until_complete(asyncio.gather(coroutine))[0]
+    deps = {dep.name: dep for dep in deps}
+    assert 'chardet' in deps
+    assert 'cryptography' not in deps
+    assert 'win-inet-pton' not in deps
+
+    coroutine = repo.get_dependencies(name='requests', version='2.21.0', extra='security')
+    deps = loop.run_until_complete(asyncio.gather(coroutine))[0]
+    deps = {dep.name: dep for dep in deps}
+    assert 'chardet' not in deps
+    assert 'win-inet-pton' not in deps
+    assert 'cryptography' in deps
