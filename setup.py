cbe3e8f78aae1030776dde8d97ed129b3716299b
Gram <master_fess@mail.ru>
2019-03-14 20:37:37 +0300

    bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='dephell',
-    version='0.2.0',
+    version='0.3.0',
     description='Dependency resolution for Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
