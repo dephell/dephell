f69766567e47271de5aef1e2d7af5f5f75b66e9d
Gram <master_fess@mail.ru>
2019-03-16 15:39:37 +0300

    show config section or value by key

--- dephell/commands/config.py
+++ dephell/commands/config.py
@@ -1,5 +1,7 @@
 import json
 from argparse import ArgumentParser
+from functools import reduce
+from operator import getitem
 
 # app
 from .base import BaseCommand
@@ -21,8 +23,22 @@ class ConfigCommand(BaseCommand):
         builders.build_venv(parser)
         builders.build_output(parser)
         builders.build_other(parser)
+        parser.add_argument('key', nargs='?')
         return parser
 
     def __call__(self):
-        print(json.dumps(self.config._data, indent=2, sort_keys=True))
+        # print all config
+        if not self.args.key:
+            print(json.dumps(self.config._data, indent=2, sort_keys=True))
+            return True
+
+        keys = self.args.key.split('-')
+        value = reduce(getitem, keys, self.config)
+        # print config section
+        if type(value) is dict:
+            print(json.dumps(value, indent=2, sort_keys=True))
+            return True
+
+        # print one value
+        print(value)
         return True
