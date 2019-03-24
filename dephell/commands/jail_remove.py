a1b90674d993a9db18f5f536da3f1f39cf8ee493
Gram <master_fess@mail.ru>
2019-03-24 11:00:49 +0300

    drop old symlinks for jail

--- dephell/commands/jail_remove.py
+++ dephell/commands/jail_remove.py
@@ -1,6 +1,7 @@
 # built-in
 import shutil
 from argparse import ArgumentParser
+from pathlib import Path
 
 # app
 from ..config import builders
@@ -29,6 +30,17 @@ class JailRemoveCommand(BaseCommand):
         if not venv.exists():
             self.logger.error('jail does not exist', extra=dict(package=name))
             return False
+
+        # remove symlinks on entrypoints
+        self.logger.info('remove executables...')
+        for entrypoint in venv.bin_path.iterdir():
+            global_entrypoint = Path(self.config['bin']) / entrypoint.name
+            if global_entrypoint.exists():
+                if global_entrypoint.resolve().samefile(entrypoint):
+                    global_entrypoint.unlink()
+                    self.logger.info('removed', extra=dict(script=entrypoint.name))
+
+        # remove venv
         shutil.rmtree(venv.path)
         self.logger.info('jail removed', extra=dict(package=name))
         return True
