91bfe720347bb9df6410bfe5e6ab978d5a11e763
Gram <master_fess@mail.ru>
2019-04-05 11:22:17 +0300

    test `inspect venv` command

---
+++ tests/test_commands/test_inspect_venv.py
@@ -0,0 +1,27 @@
+import json
+import sys
+from pathlib import Path
+
+from dephell.commands import InspectVenvCommand
+from dephell.config import Config
+from dephell.venvs import VEnv
+
+
+def test_inspect_venv_command(temp_path: Path, capsys):
+    venv = VEnv(path=temp_path)
+    venv.create(python_path=sys.executable)
+
+    config = Config()
+    config.attach({
+        'project': str(temp_path),
+        'venv': str(temp_path),
+    })
+
+    command = InspectVenvCommand(argv=[], config=config)
+    result = command()
+    assert result is True
+
+    captured = capsys.readouterr()
+    output = json.loads(captured.out)
+    assert output['exists'] is True
+    assert output['bin'] == str(venv.bin_path)
