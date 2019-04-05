1c06b6f36471237ab5dd3a466c6e5b3ad6695aca
Gram <master_fess@mail.ru>
2019-04-05 11:38:32 +0300

    test `jail install` command

---
+++ tests/test_commands/test_jail_install.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from dephell.commands import JailInstallCommand
+from dephell.config import Config
+from dephell.venvs import VEnv
+
+
+def test_jail_install_command(temp_path: Path):
+    venv_path = temp_path / 'venv'
+    bin_path = temp_path / 'bin'
+    bin_path.mkdir()
+
+    config = Config()
+    config.attach({
+        'project': str(temp_path),
+        'venv': str(venv_path),
+        'bin': str(bin_path),
+    })
+
+    command = JailInstallCommand(argv=['pycodestyle==2.5.0'], config=config)
+    result = command()
+
+    assert result is True
+    assert (bin_path / 'pycodestyle').exists()
+
+    venv = VEnv(path=venv_path)
+    assert venv.exists()
+    assert (venv.bin_path / 'pycodestyle').exists()
+    assert (venv.lib_path / 'pycodestyle-2.5.0.dist-info').exists()
