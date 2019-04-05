85e8845d8cee61bdb1cca3029fab6714f34eedf2
Gram <master_fess@mail.ru>
2019-04-05 12:01:35 +0300

    test `package downloads` command

---
+++ tests/test_commands/test_package_downloads.py
@@ -0,0 +1,22 @@
+import json
+
+from dephell.commands import PackageDownloadsCommand
+from dephell.config import Config
+
+
+def test_package_downloads_command(capsys):
+    config = Config()
+    config.attach({
+        'level': 'WARNING',
+        'silent': True,
+    })
+
+    command = PackageDownloadsCommand(argv=['DJANGO'], config=config)
+    result = command()
+
+    captured = capsys.readouterr()
+    output = json.loads(captured.out)
+    assert result is True
+    assert len(output['pythons']) > 4
+    assert len(output['systems']) > 2
+    assert '█' in output['pythons'][0]['chart']
