d1fb9936f2d44313262d890fa1e7db9d70dbf442
Gram <master_fess@mail.ru>
2019-04-05 11:07:08 +0300

    test `deps tree` command

---
+++ tests/test_commands/test_deps_tree.py
@@ -0,0 +1,27 @@
+import json
+from pathlib import Path
+
+from dephell.commands import DepsTreeCommand
+from dephell.config import Config
+
+
+def test_deps_outdated_command(temp_path: Path, capsys):
+    config = Config()
+    config.attach({
+        'level': 'WARNING',
+        'silent': True,
+    })
+
+    command = DepsTreeCommand(argv=['--type=json', 'autopep8==1.4.3'], config=config)
+    result = command()
+
+    captured = capsys.readouterr()
+    output = json.loads(captured.out)
+    assert result is True
+    assert len(output) == 2
+
+    assert output[0]['name'] == 'autopep8'
+    assert output[0]['dependencies'] == ['pycodestyle']
+
+    assert output[1]['name'] == 'pycodestyle'
+    assert output[1]['dependencies'] == []
