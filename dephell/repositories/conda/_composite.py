2e12d46682e88797166764f6f884cfd02a53568d
Gram <master_fess@mail.ru>
2019-04-28 15:59:12 +0300

    get first 5 conda deps from git

--- dephell/repositories/conda/_composite.py
+++ dephell/repositories/conda/_composite.py
@@ -14,6 +14,8 @@ class CondaRepo(CondaBaseRepo):
     git_repo = attr.ib(factory=CondaGitRepo)
     cloud_repo = attr.ib(factory=CondaCloudRepo)
 
+    _requests = attr.ib(default=0)
+
     def __attrs_post_init__(self):
         if not self.git_repo.channels:
             self.git_repo.channels = self.channels
@@ -21,10 +23,17 @@ class CondaRepo(CondaBaseRepo):
             self.cloud_repo.channels = self.channels
 
     def get_releases(self, dep) -> tuple:
-        releases = self.cloud_repo.get_releases(dep=dep)
-        if releases:
-            return releases
-        return self.git_repo.get_releases(dep=dep)
+        for repo in self._get_repos():
+            releases = repo.get_releases(dep=dep)
+            if releases:
+                return releases
+        return ()
 
     async def get_dependencies(self, *args, **kwargs):
         raise NotImplementedError('use get_releases to get deps')
+
+    def _get_repos(self):
+        if self._requests < 5:
+            return self.git_repo, self.cloud_repo
+        else:
+            return self.cloud_repo, self.git_repo
