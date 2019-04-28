b2a213c0ce2cef808d8b7ac9d36a9e79ed2cd4d7
Gram <master_fess@mail.ru>
2019-04-28 14:46:27 +0300

    preserve conda config on dump

--- dephell/converters/conda.py
+++ dephell/converters/conda.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from ruamel.yaml import YAML
 from dephell_specifier import RangeSpecifier
+from packaging.utils import canonicalize_name
 
 from ..controllers import DependencyMaker
 from ..models import RootDependency
@@ -23,15 +24,15 @@ class CondaConverter(BaseConverter):
         root = RootDependency(raw_name=doc.get('name') or self._get_name(content=content))
         repo = CondaRepo(channels=doc.get('channels', []))
         for req in doc.get('dependencies', []):
-            name, _sep, spec = req.partition('=')
-            spec = '==' + spec if spec else '*'
-            if name == 'python':
-                spec = '.'.join((spec.split('.') + ['*', '*'])[:3])
-                root.python = RangeSpecifier(spec)
+            parsed = repo.parse_req(req)
+            if parsed['name'] == 'python':
+                if parsed.get('version', '*') not in ('*', ''):
+                    spec = '.'.join((parsed['version'].split('.') + ['*', '*'])[:3])
+                    root.python = RangeSpecifier(spec)
                 continue
             root.attach_dependencies(DependencyMaker.from_params(
-                raw_name=name,
-                constraint=spec,
+                raw_name=parsed['name'],
+                constraint=parsed.get('version', '*'),
                 source=root,
                 repo=repo,
             ))
@@ -39,7 +40,7 @@ class CondaConverter(BaseConverter):
 
     def dumps(self, reqs, project: Optional[RootDependency] = None,
               content: Optional[str] = None) -> str:
-        yaml = YAML(typ='safe')
+        yaml = YAML()
         if content:
             doc = yaml.load(content)
         else:
@@ -72,17 +73,26 @@ class CondaConverter(BaseConverter):
 
         if 'dependencies' not in doc:
             doc['dependencies'] = []
-        deps = {req.name: req.version.strip('<>=') for req in reqs}
+        deps = {req.name: req.version for req in reqs}
         # remove old deps
+        represented = set()
         for index, dep in reversed(list(enumerate(doc['dependencies']))):
-            name, _sep, version = dep.partition('=')
+            parsed = CondaRepo.parse_req(dep)
+            name = canonicalize_name(parsed['name'])
+            represented.add(name)
             if name not in deps:
+                # remove
                 del doc['dependencies'][index]
+            elif parsed.get('version', '*') != deps[name]:
+                # update
+                dep = name if deps[name] in ('', '*') else '{} {}'.format(name, deps[name])
+                doc['dependencies'][index] = dep
         # add new deps
         for name, version in sorted(deps.items()):
-            dep = '{}={}'.format(name, version).rstrip('=*')
-            if dep not in doc['dependencies']:
-                doc['dependencies'].append(dep)
+            if name in represented:
+                continue
+            dep = name if version in ('', '*') else '{} {}'.format(name, version)
+            doc['dependencies'].append(dep)
         # drop empty section
         if not doc['dependencies']:
             del doc['dependencies']
