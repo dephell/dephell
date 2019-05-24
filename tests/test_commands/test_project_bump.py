b2666cfb4390909e92a458e499b97e31022a3f06
nandahkrishna <nanda.harishankar@gmail.com>
2019-05-24 22:51:23 +0530

    Test version

--- tests/test_commands/test_project_bump.py
+++ tests/test_commands/test_project_bump.py
@@ -5,6 +5,8 @@ from pathlib import Path
 # project
 from dephell.commands import ProjectBumpCommand
 from dephell.config import Config
+from dephell.actions import get_version_from_project
+from dephell_discover import Root as PackageRoot
 
 
 def test_bump_command(temp_path: Path):
@@ -12,6 +14,10 @@ def test_bump_command(temp_path: Path):
     config.attach({
         'project': str(temp_path),
     })
-    command = ProjectBumpCommand(argv=[], config=config)
+    package = PackageRoot(path=Path(config['project']))
+    old_version = get_version_from_project(package)
+    command = ProjectBumpCommand(argv=['serial'], config=config)
     result = command()
+    new_version = get_version_from_project(package)
     assert result is True
+    assert old_version != new_version
