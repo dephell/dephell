e9d4c0c5e77881a6082f0f10ca1bc15ce443bc20
Gram <master_fess@mail.ru>
2019-07-02 14:37:29 +0200

    allow to pass paths into load_resolver

--- dephell/converters/base.py
+++ dephell/converters/base.py
@@ -64,8 +64,13 @@ class BaseConverter:
         root = self.loads(content)
         return self._get_resolver(root)
 
-    def load_resolver(self, path) -> Resolver:
-        root = self.load(path)
+    def load_resolver(self, path=None, paths=None) -> Resolver:
+        if path is not None:
+            root = self.load(path=path)
+        elif paths is not None:
+            root = self.load(paths=paths)
+        else:
+            raise NameError('path is required')
         return self._get_resolver(root)
 
     # helpers
