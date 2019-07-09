f2afed2dc3c63103c3b53a4e23dbdfffa2642799
Gram <master_fess@mail.ru>
2019-07-09 16:19:49 +0200

    docker cd in workdir

--- dephell/templates/docker_prepare.sh
+++ dephell/templates/docker_prepare.sh
@@ -3,11 +3,11 @@ apt-get update -yq
 apt-get install -yq zsh python3 python3-dev python3-pip mc glances zsh-syntax-highlighting wget
 
 # oh my zsh
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-chmod +x install.sh
-yes | ./install.sh --silent
+wget -O /tmp/install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+chmod +x /tmp/install.sh
+yes | /tmp/install.sh --silent
 echo "\nplugins=(colorize command-not-found django celery git pip pipenv python)\n" >> ~/.zshrc
-rm install.sh
+rm /tmp/install.sh
 
 # pure zsh theme
 wget https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh
@@ -18,8 +18,8 @@ echo "\nautoload -U promptinit\npromptinit\nprompt pure\n" >> ~/.zshrc
 
 # dephell
 python3 -m pip install -U pip
-wget https://raw.githubusercontent.com/dephell/dephell/master/install.py
+wget -O /tmp/install.py https://raw.githubusercontent.com/dephell/dephell/master/install.py
 # pip can't update itself from the first try >.<
-bash -c "python3 install.py || python3 install.py"
-rm install.py
+bash -c "python3 /tmp/install.py || python3 /tmp/install.py"
+rm /tmp/install.py
 dephell autocomplete
