0ddc7b1f537cea9a081ba4cf693762a1c45ebb88
Gram <master_fess@mail.ru>
2019-07-11 16:46:15 +0200

    update converter interface to make possible specify project path

--- dephell/converters/base.py
+++ dephell/converters/base.py
@@ -2,40 +2,55 @@
 from os import unlink
 from pathlib import Path
 from tempfile import NamedTemporaryFile
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Union
+
+import attr
 
 # app
 from ..controllers import Graph, Mutator, Resolver
 from ..models import RootDependency
 
 
+@attr.s()
 class BaseConverter:
-    lock = False
+    lock = attr.ib(type=bool, default=False)
+    project_path = attr.ib(type=Optional[Path], default=None)
+    resolve_path = attr.ib(type=Optional[Path], default=None)
+    default_filename = attr.ib(type=Optional[str], default=None)
 
     # inspection
 
     def can_parse(self, path: Path, content: Optional[str] = None) -> bool:
         return False
 
+    def copy(self, **kwargs) -> 'BaseConverter':
+        params = attr.asdict(self, recurse=False)
+        params.update(kwargs)
+        return type(self)(**params)
+
     # root dependency load and dump
 
     def loads(self, content: str) -> RootDependency:
+        """read dependencies from text
+        """
         stream = NamedTemporaryFile(mode='w', delete=False)
         stream.write(content)
         stream.close()
-        root = self.load(stream.name)
+        root = self.load(path=stream.name)
         unlink(stream.name)
         return root
 
-    def load(self, path) -> RootDependency:
+    def load(self, path: Union[Path, str]) -> RootDependency:
+        """read dependencies from file
+        """
         path = Path(str(path))
         with path.open('r', encoding='utf8') as stream:
-            return self.loads(stream.read())
+            return self.loads(content=stream.read())
 
-    def dumps(self, reqs, project: RootDependency, content: Optional[str] = None) -> str:
+    def dumps(self, reqs, *, project: RootDependency, content: str = None) -> str:
         raise NotImplementedError
 
-    def dump(self, reqs, path, project: RootDependency) -> None:
+    def dump(self, reqs, *, path, project: RootDependency) -> None:
         # read
         path = Path(str(path))
         if path.exists():
@@ -79,3 +94,26 @@ class BaseConverter:
         if not extra:
             extra = 'main'
         return extra, marker
+
+    @staticmethod
+    def _make_path_absolute(root: Path, path: Path) -> Path:
+        if path.is_absolute():
+            return path
+        root = root.resolve()
+        return (root / path).resolve()
+
+    def _make_source_path_absolute(self, path: Path) -> Path:
+        if self.project_path is not None:
+            root = self.project_path
+        else:
+            root = Path()
+        return self._make_path_absolute(root=root, path=path)
+
+    def _make_dependency_path_absolute(self, path: Path) -> Path:
+        if self.resolve_path is not None:
+            root = self.resolve_path
+        elif self.project_path is not None:
+            root = self.project_path
+        else:
+            root = Path()
+        return self._make_path_absolute(root=root, path=path)
