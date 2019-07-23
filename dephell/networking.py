5e889beaf230f27d3106b4ac044d501654cf5b99
Hugo <hugovk@users.noreply.github.com>
2019-07-23 13:45:13 +0300

    User-Agent must be a string

--- dephell/networking.py
+++ dephell/networking.py
@@ -9,7 +9,7 @@ from aiohttp import ClientSession, TCPConnector
 # app
 from . import __version__
 
-USER_AGENT = {'User-Agent': 'DepHell/{version}'.format(version=__version__)}
+USER_AGENT = 'DepHell/{version}'.format(version=__version__)
 
 
 def aiohttp_session(*, auth=None, **kwargs):
