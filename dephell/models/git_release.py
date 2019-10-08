1f88cd7b2aab39a5fa7616e8508826332a1dc77c
Gram <master_fess@mail.ru>
2019-10-08 14:27:15 +0200

    use custom compare

--- dephell/models/git_release.py
+++ dephell/models/git_release.py
@@ -5,6 +5,25 @@ import attr
 from .release import Release
 
 
-@attr.s(hash=False, eq=True, order=True)
+@attr.s(hash=False, eq=False, order=False)
 class GitRelease(Release):
     commit = attr.ib(default=None)  # just for information
+
+    def __eq__(self, other) -> str:
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        if self.name != other.name:
+            return False
+        if self.commit != other.commit:
+            return False
+        return True
+
+    def __lt__(self, other):
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        left = (self.name, self.commit, self.version, self.time)
+        right = (other.name, self.commit, other.version, other.time)
+        return left < right
+
+    def __hash__(self) -> int:
+        return hash((self.name, self.commit, self.version))
