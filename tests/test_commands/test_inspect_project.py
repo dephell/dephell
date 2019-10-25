610d23b7f911c0833081385409c93b9fb2f7c46a
Jai Agarwal <jai.bhageria@gmail.com>
2019-10-26 04:44:39 +0530

    modified tests

--- tests/test_commands/test_inspect_project.py
+++ tests/test_commands/test_inspect_project.py
@@ -8,10 +8,13 @@ from dephell.commands import InspectProjectCommand
 from dephell.config import Config
 
 
-def test_inspect_project_command(temp_path: Path, capsys):
+def test_inspect_project_command(temp_path: Path, requirements_path: Path, capsys):
+    from_path = str(requirements_path / 'poetry.toml')
     config = Config()
     config.attach({
-        'project': 'nani',
+        'from': {'format': 'poetry', 'path': from_path},
+        'to': {'format': 'setuppy', 'path': 'stdout'},
+        'project': str(temp_path),
         'nocolors': True,
     })
 
@@ -20,5 +23,6 @@ def test_inspect_project_command(temp_path: Path, capsys):
     assert result is True
 
     captured = capsys.readouterr()
+    print(captured.out)
     output = json.loads(captured.out)
-    assert output['project_name']=='nani' 
+    assert output['project_name']=='my-package' 
