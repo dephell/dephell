5ed2e4b92ffd02b152c52b73b8d975f08d655fbe
Gram <master_fess@mail.ru>
2019-11-14 16:18:52 +0100

    use dephell_setuptools

--- dephell/converters/setuppy.py
+++ dephell/converters/setuppy.py
@@ -1,26 +1,21 @@
 # built-in
-import sys
 from collections import defaultdict
-from distutils.core import run_setup
 from json import dumps as json_dumps
 from logging import getLogger
 from pathlib import Path
-from re import sub
 from typing import Optional
 
 # external
 from dephell_discover import Root as PackageRoot
 from dephell_links import DirLink, FileLink, URLLink, VCSLink, parse_link
 from dephell_specifier import RangeSpecifier
+from dephell_setuptools import read_setup
 from packaging.requirements import Requirement
-from setuptools.dist import Distribution
 
 # app
-from ..context_tools import chdir
 from ..controllers import DependencyMaker, Readme
 from ..models import Author, EntryPoint, RootDependency
 from .base import BaseConverter
-
 
 try:
     from yapf.yapflib.style import CreateGoogleStyle
@@ -77,65 +72,57 @@ class SetupPyConverter(BaseConverter):
         path = self._make_source_path_absolute(path)
         self._resolve_path = path.parent
 
-        old_sys_path = sys.path
-        sys.path.append(str(path.parent))
-
-        info = self._execute(path=path)
-        if info is None:
-            with chdir(path.parent):
-                info = run_setup(path.name)
-
-        sys.path = old_sys_path
+        data = read_setup(path=path, error_handler=logger.debug)
 
         root = RootDependency(
-            raw_name=self._get(info, 'name'),
-            version=self._get(info, 'version') or '0.0.0',
+            raw_name=data['name'],
+            version=data.get('version', '0.0.0'),
             package=PackageRoot(
                 path=self.project_path or Path(),
-                name=self._get(info, 'name') or None,
+                name=data['name'],
             ),
 
-            description=self._get(info, 'description'),
-            license=self._get(info, 'license'),
+            description=data.get('description'),
+            license=data.get('license'),
 
-            keywords=tuple(self._get_list(info, 'keywords')),
-            classifiers=tuple(self._get_list(info, 'classifiers')),
-            platforms=tuple(self._get_list(info, 'platforms')),
+            keywords=tuple(data.get('keywords', ())),
+            classifiers=tuple(data.get('classifiers', ())),
+            platforms=tuple(data.get('platforms', ())),
 
-            python=RangeSpecifier(self._get(info, 'python_requires')),
+            python=RangeSpecifier(data.get('python_requires')),
             readme=Readme.from_code(path=path),
         )
 
         # links
         for key, name in (('home', 'url'), ('download', 'download_url')):
-            link = self._get(info, name)
+            link = data.get(name)
             if link:
                 root.links[key] = link
 
         # authors
         for name in ('author', 'maintainer'):
-            author = self._get(info, name)
+            author = data.get(name)
             if author:
                 root.authors += (
-                    Author(name=author, mail=self._get(info, name + '_email')),
+                    Author(name=author, mail=data.get(name + '_email')),
                 )
 
         # entrypoints
         entrypoints = []
-        for group, content in (getattr(info, 'entry_points', {}) or {}).items():
+        for group, content in data.get('entry_points', {}).items():
             for entrypoint in content:
                 entrypoints.append(EntryPoint.parse(text=entrypoint, group=group))
         root.entrypoints = tuple(entrypoints)
 
         # dependency_links
         urls = dict()
-        for url in self._get_list(info, 'dependency_links'):
+        for url in data.get('dependency_links', ()):
             parsed = parse_link(url)
             name = parsed.name.split('-')[0]
             urls[name] = url
 
         # dependencies
-        for req in self._get_list(info, 'install_requires'):
+        for req in data.get('install_requires', ()):
             req = Requirement(req)
             root.attach_dependencies(DependencyMaker.from_requirement(
                 source=root,
@@ -144,7 +131,7 @@ class SetupPyConverter(BaseConverter):
             ))
 
         # extras
-        for extra, reqs in getattr(info, 'extras_require', {}).items():
+        for extra, reqs in data.get('extras_require', {}).items():
             extra, marker = self._split_extra_and_marker(extra)
             envs = {extra} if extra == 'dev' else {'main', extra}
             for req in reqs:
@@ -259,65 +246,6 @@ class SetupPyConverter(BaseConverter):
         return content
 
     # private methods
-
-    @staticmethod
-    def _execute(path: Path):
-        source = path.read_text('utf-8')
-        # Remove any dotted module names
-        new_source = sub(r'[a-z][a-z0-9.]*\.setup\(', 'setup(', source)
-        # Remove return
-        new_source = new_source.replace('return setup(', 'setup(')
-        # Rename functions that end with setup
-        new_source = sub(r'([_a-z][a-z0-9._]*)setup\(', r'setup\1(', new_source)
-        # Ensure _dist is global
-        new_source = new_source.replace('setup(', 'global _dist; _dist = dict(')
-        if new_source == source:
-            logger.error('cannot modify source')
-            return None
-
-        globe = {
-            '__file__': str(path),
-            '__name__': '__main__',
-        }
-        with chdir(path.parent):
-            try:
-                exec(compile(new_source, path.name, 'exec'), globe)
-            except Exception as e:
-                logger.exception('_execute {}: {}'.format(type(e).__name__, str(e)))
-
-        dist = globe.get('_dist')
-        if dist is None:
-            logger.error('distribution was not called')
-            return None
-        return Distribution(dist)
-
-    @staticmethod
-    def _get(msg, name: str) -> str:
-        value = getattr(msg.metadata, name, None)
-        if not value:
-            value = getattr(msg, name, None)
-        if not value:
-            return ''
-        value = str(value)
-        if value == 'UNKNOWN':
-            return ''
-        return value.strip()
-
-    @staticmethod
-    def _get_list(msg, name: str) -> tuple:
-        if name == 'keywords':
-            return ' '.join(msg.get_keywords()).split()
-
-        getter = getattr(msg, 'get_' + name, None)
-        if getter:
-            values = getter()
-        else:
-            values = getattr(msg, name, None)
-        if type(values) is str:
-            values = values.split()
-        if not values:
-            return ()
-        return tuple(value for value in values if value != 'UNKNOWN' and value.strip())
 
     @staticmethod
     def _format_req(req) -> str:
