bc092546abdd3f844992f2d545ecf45fc84f6b13
Gram <master_fess@mail.ru>
2019-12-04 14:37:14 +0100

    test _get_url

---
+++ tests/test_commands/test_package_bug.py
@@ -0,0 +1,25 @@
+import pytest
+from dephell.commands import PackageBugCommand
+
+
+@pytest.mark.parametrize('links, url', [
+    (
+        {'home': 'https://bitbucket.org/saaj/torrelque'},
+        'https://bitbucket.org/saaj/torrelque/issues/new',
+    ),
+    (
+        {'repository': 'https://github.com/dephell/dephell'},
+        'https://github.com/dephell/dephell/issues/new',
+    ),
+    (
+        {'home': 'https://gitlab.com/pycqa/flake8'},
+        'https://gitlab.com/pycqa/flake8/issues/new',
+    ),
+    (
+        {'Tracker': 'https://code.djangoproject.com/'},
+        'https://code.djangoproject.com/',
+    ),
+])
+def test_get_url(requests_mock, links, url):
+    requests_mock.head(url)
+    assert PackageBugCommand._get_url(links=links) == url
